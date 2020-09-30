11dd2f6819a90863dc766a21b02196c2c018ba20
Zach Cossman <Zachcos@gmail.com>
2020-09-30 01:58:00 -0400

    Create globalStyle file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import GlobalStyle from './imports/globalStyle';
 
 function App() {
   return (
     <div className="App">
       <h1>hello, world!</h1>
+      <GlobalStyle /> 
     </div>
   );
 }
