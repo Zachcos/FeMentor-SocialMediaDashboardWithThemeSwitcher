b0042012db5c914ad9a63e263a2935c2332f2d1e
Zach Cossman <Zachcos@gmail.com>
2020-09-30 03:50:17 -0400

    Add Google Fonts link

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,8 +1,28 @@
 import reset from 'styled-reset';
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
+
+${({ theme }) => css`
+  body {
+    background-color: ${theme.bg};
+    color: ${theme.text_light};
+    font-family: "Inter", sans-serif;
+
+    &:before {
+      background-color: ${theme.topBgPattern};
+      content: "";
+      height: 245px;
+      left: 0;
+      position: absolute;
+      right: 0;
+      top: 0;
+      z-index: -1;
+    }
+  }
+`
+}
 
 html {
   --limeGreen: #1db489;
