9679a0d22edf9f70196decc4cf21f72dcb46bec2
Zach Cossman <Zachcos@gmail.com>
2020-09-30 03:28:25 -0400

    Remove import and rendering of Test component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {AppProvider} from './app-context';
-import Test from './test';
 
 function App() {
   return (
     <AppProvider>
       <div className="App">
         <h1>hello, world!</h1>
-        <Test />
       </div>
     </AppProvider>
   );
