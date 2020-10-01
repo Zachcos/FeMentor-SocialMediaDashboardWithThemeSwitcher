00f68c0525a38dcb3c59a768af626baa8f51475b
Zach Cossman <Zachcos@gmail.com>
2020-09-30 20:55:43 -0400

    Fix globalStyle import in app-context

--- src/app-context.js
+++ src/app-context.js
@@ -1,25 +1,25 @@
 import React, { useState, createContext } from 'react';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './globalStyle';
+import { GlobalStyle } from './globalStyle';
 import initialState from './models/app-state';
 import themes from './models/themes';
 
 const AppStateContext = createContext(initialState);
 // const AppDispatchContext = createContext(); //TODO: Wire up and find out why this is it's own context
 
-const AppProvider = ({children}) => {
+const AppProvider = ({ children }) => {
   const [currentTheme, setTheme] = useState(initialState.currentTheme);
 
   const theme = themes[currentTheme];
 
   return (
-    <AppStateContext.Provider value={{...initialState}}>
+    <AppStateContext.Provider value={{ ...initialState }}>
       <ThemeProvider theme={theme}>
         {children}
-        <GlobalStyles />
+        <GlobalStyle />
       </ThemeProvider>
     </AppStateContext.Provider>
-  )
-}
+  );
+};
 
-export { AppProvider, AppStateContext};
+export { AppProvider, AppStateContext };
