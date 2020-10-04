48a0a881f161757ae8c39984fba06cfea3d176cf
Zach Cossman <Zachcos@gmail.com>
2020-10-03 22:16:46 -0400

    Fix dark theme toggleBg value

--- src/models/themes.js
+++ src/models/themes.js
@@ -9,7 +9,8 @@ const themes = {
     subheadingColor: 'hsl(228, 34%, 66%)',
     textColor: 'hsl(0, 0%, 100%)',
     hrBg: 'hsl(232, 19%, 23%)',
-    toggleBg: 'linear gradient hsl(210, 78%, 56%) to hsl(146, 68%, 55%)',
+    toggleBg:
+      'linear-gradient(90deg, hsl(210, 78%, 56%) 0%, hsl(146, 68%, 55%) 100%)',
     toggleCircleColor: 'hsl(228, 28%, 20%)',
   },
   light: {
@@ -22,7 +23,8 @@ const themes = {
     subheadingColor: 'hsl(228, 12%, 44%)',
     textColor: 'hsl(230, 17%, 14%)',
     hrBg: 'hsl(229, 10%, 67%)',
-    toggleBg: 'linear-gradient(0deg, #aeb3cb 0%, #aeb3cb 100%)',
+    toggleBg:
+      'linear-gradient(0deg, hsl(232, 22%, 74%) 0%, hsl(232, 22%, 74%) 100%)',
     toggleCircleColor: 'hsl(0, 0%, 100%)',
   },
 };
