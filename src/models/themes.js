b54c0ccba8f08141ac3a802b485edad1b92caee3
Zach Cossman <Zachcos@gmail.com>
2020-10-03 21:09:55 -0400

    Refactor theme colors for more descriptive names

--- src/models/themes.js
+++ src/models/themes.js
@@ -1,20 +1,30 @@
 const themes = {
   dark: {
     bg: 'hsl(230, 17%, 14%)',
-    topBgPattern: 'hsl(232, 19%, 15%)',
+    topBg: 'hsl(232, 19%, 15%)',
     cardBg: 'hsl(228, 28%, 20%)',
-    text_light: 'hsl(0, 0%, 100%)',
-    text_dark: 'hsl(228, 34%, 66%)',
-    toggle: 'linear gradient hsl(210, 78%, 56%) to hsl(146, 68%, 55%)',
+    cardHoverBg: 'hsl(228, 29%, 20%)',
+    headingColor: 'hsl(0, 0%, 100%)',
+    sectionHeadingColor: 'hsl(0, 0%, 100%)',
+    subheadingColor: 'hsl(228, 34%, 66%)',
+    textColor: 'hsl(0, 0%, 100%)',
+    hrBg: 'hsl(232, 19%, 23%)',
+    toggleBg: 'linear gradient hsl(210, 78%, 56%) to hsl(146, 68%, 55%)',
+    toggleCircleColor: 'hsl(228, 28%, 20%)',
   },
   light: {
-    bg: 'hsl(0, 0%, 100%)',
-    topBgPattern: 'hsl(225, 100%, 98%)',
+    bodyBg: 'hsl(0, 0%, 100%)',
+    topBg: 'hsl(225, 100%, 98%)',
     cardBg: 'hsl(227, 47%, 96%)',
-    text_light: 'hsl(228, 12%, 44%)',
-    text_dark: 'hsl(230, 17%, 14%)',
-    toggle: 'hsl(230, 22%, 74%)',
-  }
-}
+    cardHoverBg: 'hsl(232, 33%, 91%)',
+    headingColor: 'hsl(230, 17%, 14%)',
+    sectionHeadingColor: 'hsl(228, 12%, 44%)',
+    subheadingColor: 'hsl(228, 12%, 44%)',
+    textColor: 'hsl(230, 17%, 14%)',
+    hrBg: 'hsl(229, 10%, 67%)',
+    toggleBg: 'linear-gradient(0deg, #aeb3cb 0%, #aeb3cb 100%)',
+    toggleCircleColor: 'hsl(0, 0%, 100%)',
+  },
+};
 
 export default themes;
