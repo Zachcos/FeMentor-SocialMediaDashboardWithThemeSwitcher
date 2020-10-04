2c4324247ec2266a19c07cbd05e88718c9c1e00e
Zach Cossman <Zachcos@gmail.com>
2020-10-04 00:14:57 -0400

    Add Container styled-component

--- src/components/dashboard-subscription.js
+++ src/components/dashboard-subscription.js
@@ -4,13 +4,24 @@ import styled from 'styled-components';
 import { AppStateContext } from '../app-context';
 import { SectionHeading } from './index';
 
+const Container = styled.section`
+  display: grid;
+  column-gap: 1.875rem;
+  row-gap: 1.5rem;
+  grid-template-columns: repeat(auto-fit, minmax(15.9375rem, 1fr));
+
+  ${SectionHeading} {
+    grid-column: 1 / -1;
+  }
+`;
+
 export const DashboardSubscrption = () => {
   console.log('This is the subscription dashboard');
 
   return (
-    <div className="container">
+    <Container>
       <SectionHeading>Overview - Today</SectionHeading>
       <p>data will go here</p>
-    </div>
+    </Container>
   );
 };
