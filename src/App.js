e9dc38ce60d8bfbae5ab0c62c23badc187c83526
Zach Cossman <Zachcos@gmail.com>
2020-10-03 21:24:58 -0400

    Import and render DashboardHeader component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import { AppProvider } from './app-context';
+
+import { DashboardHeader } from './components/index';
+
+const Container = styled.div`
+  max-width: 69.375rem;
+  margin: 0 auto;
+  padding: 2.25rem 1.5rem;
+`;
 
 function App() {
   return (
     <AppProvider>
-      <div className="App">
-        <h1>hello, world!</h1>
-      </div>
+      <Container>
+        <DashboardHeader />
+      </Container>
     </AppProvider>
   );
 }
